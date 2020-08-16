073a86f2d6f0609655278f4e11431f4c1ac22262
Manami Ueda <manamiueda1@gmail.com>
2020-08-16 19:22:42 -0400

    no errors

--- Easy/twoSum.js
+++ Easy/twoSum.js
@@ -4,35 +4,41 @@
 
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; i < nums.length; j++) {
-      if (i + j === target) {
-        return [i, j];
+    for (let j = i + 1; j < nums.length; j++) {
+      console.log(i, j);
+      if (nums[i] + nums[j] === target) {
+        console.log([i, j]);
+        return;
       }
     }
   }
-  return false;
+  console.log(false);
+  return;
 };
+
+twoSum([1, 2, 4, 5, 9], 10);
+twoSum([1, 2, 3], 100);
 
 // Time Complexity: O(n^2)
 // Space Complexity: O(1)
 
 // optimized solution
 
-function twoNumberSum(array, targetSum) {
-  // Write your code here.
-  array.sort((a, b) => a - b);
-  let left = 0;
-  let right = array.length - 1;
+// function twoNumberSum(array, targetSum) {
+//   // Write your code here.
+//   array.sort((a, b) => a - b);
+//   let left = 0;
+//   let right = array.length - 1;
 
-  while (left < right) {
-    let currentSum = array[left] + array[right];
-    if (currentSum === targetSum) {
-      return [array[left], array[right]];
-    } else if (currentSum < targetSum) {
-      left++;
-    } else if (currentSum > targetSum) {
-      right--;
-    }
-  }
-  return [];
-}
+//   while (left < right) {
+//     let currentSum = array[left] + array[right];
+//     if (currentSum === targetSum) {
+//       return [array[left], array[right]];
+//     } else if (currentSum < targetSum) {
+//       left++;
+//     } else if (currentSum > targetSum) {
+//       right--;
+//     }
+//   }
+//   return [];
+// }
