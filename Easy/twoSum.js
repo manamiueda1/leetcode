a509717ce3b2d6592a35044783d3a3e7cdeccb6e
Manami Ueda <manamiueda1@gmail.com>
2020-08-16 20:38:03 -0400

    twoSum is done

--- Easy/twoSum.js
+++ Easy/twoSum.js
@@ -2,6 +2,8 @@
 
 // Brut Force Solution
 
+// Time: O(n^2) Space: O(1)
+
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
@@ -19,26 +21,40 @@ var twoSum = function (nums, target) {
 twoSum([1, 2, 4, 5, 9], 10);
 twoSum([1, 2, 3], 100);
 
-// Time Complexity: O(n^2)
-// Space Complexity: O(1)
-
 // optimized solution
+// Time: O(n) Space: O(1)
 
-// function twoNumberSum(array, targetSum) {
-//   // Write your code here.
-//   array.sort((a, b) => a - b);
-//   let left = 0;
-//   let right = array.length - 1;
+function twoNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
 
-//   while (left < right) {
-//     let currentSum = array[left] + array[right];
-//     if (currentSum === targetSum) {
-//       return [array[left], array[right]];
-//     } else if (currentSum < targetSum) {
-//       left++;
-//     } else if (currentSum > targetSum) {
-//       right--;
-//     }
-//   }
-//   return [];
-// }
+  while (left < right) {
+    let currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left], array[right]];
+    } else if (currentSum < targetSum) {
+      left++;
+    } else if (currentSum > targetSum) {
+      right--;
+    }
+  }
+  return [];
+}
+
+// Solution using Hash Map
+// time: 0(n) space: 0(n)
+
+function twoNumberSum(array, targetSum) {
+  let track = new Map();
+  for (let i = 0; i < array.length; i++) {
+    let remainder = targetSum - array[i];
+
+    if (track[remainder]) {
+      return [array[i], remainder];
+    } else {
+      track[array[i]] = true;
+    }
+  }
+  return [];
+}
